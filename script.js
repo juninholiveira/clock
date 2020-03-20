43f76f00f08a545fa8f7e5ffccae84bc13497e6a
juninholiveira <leandro-nim@hotmail.com>
2020-03-19 23:18:59 -0300

    comments added

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
+//Esse método chama a função setClock de 1 em 1 segundo (1000ms)
 setInterval(setClock, 1000)
 
+//Busca no documento e salva nas constantes os elementos físicos dos ponteiros no HTML
 const secondHand = document.querySelector('[data-second-hand]')
 const minuteHand = document.querySelector('[data-minute-hand]')
 const hourHand = document.querySelector('[data-hour-hand]')
 
+//Essa função serve para definir quais os valores de cada ponteiro
 function setClock()
 {
-    const currentDate = new Date()
-    const secondsRatio = currentDate.getSeconds() / 60
-    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
-    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+    const currentDate = new Date()                                          //Cria um novo objeto Date que pega o valor de tempo atual do sistema
+    const secondsRatio = currentDate.getSeconds() / 60                      //Pega o valor de segundos atuais e divide por 60 para obter um valor francionário entre 0 e 1
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60     //Pega o valor de minutos atuais e divide por 60 para obter a fração de onde o ponteiro deve estar, mas adiciona os segundos para fazer isso gradualmente
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12         //Pega o valor de horas atuais e divide por 60 para obter a fração de onde o ponteiro deve estar, mas adiciona os minutos para fazer isso gradualmente
 
-    setRotation(secondHand, secondsRatio)
-    setRotation(minuteHand, minutesRatio)
-    setRotation(hourHand, hoursRatio)
+    setRotation(secondHand, secondsRatio)                                   //Chama a função 'setRotation' e passa o ponteiro e o valor que ele deve estar
+    setRotation(minuteHand, minutesRatio)                                   //Chama a função 'setRotation' e passa o ponteiro e o valor que ele deve estar
+    setRotation(hourHand, hoursRatio)                                       //Chama a função 'setRotation' e passa o ponteiro e o valor que ele deve estar
 }
 
+//Essa função serve para passar para os ponteiros físicos do HTML a posição de cada um.
+//Ela recebe como parametro o ponteiro que vai ser modificado e o valor que ele deve estar
 function setRotation(element, rotationRatio)
 {
-    element.style.setProperty('--rotation', rotationRatio * 360)
+    element.style.setProperty('--rotation', rotationRatio * 360)            //Como o valor que recebo é entre 0 e 1, multiplico por 360 para achar o angulo certo dele
 }
 
+//Chama essa função uma vez no início para não precisar esperar 1 segundo inteiro para o relógio atualizar ao carregar a página
 setClock()
